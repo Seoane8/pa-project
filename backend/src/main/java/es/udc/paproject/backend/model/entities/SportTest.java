a19165fb6c367dadfb5c65fdf106cd94325a2af2
Adrián <adrian.mcastelo@udc.es>
2021-03-02 20:06:20 +0100

    [VARIOS] Añadida entidad SportTest y SportTestDao

---
+++ backend/src/main/java/es/udc/paproject/backend/model/entities/SportTest.java
@@ -0,0 +1,132 @@
+package es.udc.paproject.backend.model.entities;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class SportTest {
+
+    private Long id;
+    private String name;
+    private String description;
+    private LocalDateTime date;
+    private float price;
+    private int maxParticipants;
+    private int numParticipants;
+    private SportTestType type;
+    private String location;
+    private Province province;
+    private int rating;
+
+    public SportTest() {}
+
+    public SportTest(Long id, String name, String description, LocalDateTime date, float price,
+                     int maxParticipants, int numParticipants, SportTestType type, String location,
+                     Province province, int rating) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.date = date;
+        this.price = price;
+        this.maxParticipants = maxParticipants;
+        this.numParticipants = numParticipants;
+        this.type = type;
+        this.location = location;
+        this.province = province;
+        this.rating = rating;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getMaxParticipants() {
+        return maxParticipants;
+    }
+
+    public void setMaxParticipants(int maxParticipants) {
+        this.maxParticipants = maxParticipants;
+    }
+
+    public int getNumParticipants() {
+        return numParticipants;
+    }
+
+    public void setNumParticipants(int numParticipants) {
+        this.numParticipants = numParticipants;
+    }
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "SportTestTypeId")
+    public SportTestType getType() {
+        return type;
+    }
+
+    public void setType(SportTestType type) {
+        this.type = type;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "ProvinceId")
+    public Province getProvince() {
+        return province;
+    }
+
+    public void setProvince(Province province) {
+        this.province = province;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+}
