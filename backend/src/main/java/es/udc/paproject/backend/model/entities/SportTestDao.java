4daebbcb0fc185b62f3bb8465b1ffcc90124e231
Adrián <adrian.mcastelo@udc.es>
2021-03-02 20:33:33 +0100

    [VARIOS] Solución interfaces SportTest

--- backend/src/main/java/es/udc/paproject/backend/model/entities/SportTestDao.java
+++ backend/src/main/java/es/udc/paproject/backend/model/entities/SportTestDao.java
@@ -1,11 +1,5 @@
 package es.udc.paproject.backend.model.entities;
 
-import org.springframework.data.domain.Slice;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-import java.time.LocalDate;
-
-public interface SportTestDao {
-
-    Slice<SportTest> findRaces(Province province, SportTestType type, LocalDate startDate, LocalDate finishDate);
-
-}
+public interface SportTestDao extends PagingAndSortingRepository<SportTest, Long>, CustomizedSportTestDao {}
