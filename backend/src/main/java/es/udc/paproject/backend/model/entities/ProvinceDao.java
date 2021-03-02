79c5c63b7ab1a43c5dd804c408e6e988595150b2
brais.cibeira <brais.cibeira@udc.es>
2021-03-02 19:49:52 +0100

    [VARIOS] Añadidas entidades Province y SportTestType

---
+++ backend/src/main/java/es/udc/paproject/backend/model/entities/ProvinceDao.java
@@ -0,0 +1,6 @@
+package es.udc.paproject.backend.model.entities;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ProvinceDao extends PagingAndSortingRepository<Province, Long> {
+}
