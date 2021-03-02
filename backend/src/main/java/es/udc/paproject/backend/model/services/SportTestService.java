24a48a20951f54a492fcb77b1c8188823e035532
Adrián <adrian.mcastelo@udc.es>
2021-03-02 21:03:24 +0100

    [BUS-PRU] [INF-PRU] Definición de la interfaz para dichos casos de uso

--- backend/src/main/java/es/udc/paproject/backend/model/services/SportTestService.java
+++ backend/src/main/java/es/udc/paproject/backend/model/services/SportTestService.java
@@ -1,4 +1,21 @@
 package es.udc.paproject.backend.model.services;
 
+import es.udc.paproject.backend.model.entities.Province;
+import es.udc.paproject.backend.model.entities.SportTest;
+import es.udc.paproject.backend.model.entities.SportTestType;
+import es.udc.paproject.backend.model.exceptions.InstanceNotFoundException;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public interface SportTestService {
+
+    List<Province> findAllProvinces();
+
+    List<SportTestType> findAllSportTestTypes();
+
+    SportTest findSportTestById(Long id) throws InstanceNotFoundException;
+
+    Block<SportTest> findSportTests(Province province, SportTestType type, LocalDate startDate, LocalDate finishDate, int page, int size);
+
 }
