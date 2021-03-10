b76d87c0b780c38389906bbb32606a9754e508d8
Diego Seoane <diegoseoanegonzalez8@gmail.com>
2021-03-10 13:50:37 +0100

    [VARIOS] Añadido script de creación de tablas de la base de datos

--- backend/src/sql/1-MySQLCreateTables.sql
+++ backend/src/sql/1-MySQLCreateTables.sql
@@ -1,4 +1,8 @@
 DROP TABLE User;
+DROP TABLE Province;
+DROP TABLE SportTestType;
+DROP TABLE SportTest;
+DROP TABLE Inscription;
 
 CREATE TABLE User (
     id BIGINT NOT NULL AUTO_INCREMENT,
@@ -13,3 +17,52 @@ CREATE TABLE User (
 ) ENGINE = InnoDB;
 
 CREATE INDEX UserIndexByUserName ON User (userName);
+
+CREATE TABLE Province (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(60) NOT NULL,
+    CONSTRAINT ProvincePK PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+CREATE TABLE SportTestType (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(60) NOT NULL,
+    CONSTRAINT ProvincePK PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+CREATE TABLE SportTest (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(60) NOT NULL,
+    description VARCHAR(2000) NOT NULL,
+    date DATETIME NOT NULL,
+    price DECIMAL(11, 2) NOT NULL,
+    maxParticipants INT NOT NULL,
+    numParticipants INT NOT NULL,
+    sportTestTypeId BIGINT NOT NULL,
+    location VARCHAR(60) NOT NULL,
+    provinceId BIGINT NOT NULL,
+    rating INT,
+    version INT NOT NULL,
+    CONSTRAINT SportTestPK PRIMARY KEY (id),
+    CONSTRAINT SportTestSportTestTypeIdFK FOREIGN KEY (sportTestTypeId)
+        REFERENCES SportTestType (id),
+    CONSTRAINT SportTestProvinceId FOREIGN KEY (provinceId)
+        REFERENCES Province (id)
+) ENGINE = InnoDB;
+
+CREATE TABLE Inscription (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    userId BIGINT NOT NULL,
+    sportTestId BIGINT NOT NULL,
+    cardNumber VARCHAR(20) NOT NULL,
+    reservationDate DATETIME NOT NULL,
+    price DECIMAL(11, 2) NOT NULL,
+    dorsal INT NOT NULL,
+    dorsalCollected BOOLEAN NOT NULL,
+    score INT,
+    CONSTRAINT InscriptionPK PRIMARY KEY (id),
+    CONSTRAINT InscriptionUserIdFK FOREIGN KEY (userId)
+        REFERENCES User (id),
+    CONSTRAINT InscriptionSportTestIdFK FOREIGN KEY (sportTestId)
+        REFERENCES SportTest (id)
+) ENGINE = InnoDB;
