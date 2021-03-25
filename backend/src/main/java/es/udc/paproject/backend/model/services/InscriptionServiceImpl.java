72bab2e053efbffc0dee0384cb3bd43e71885bc3
Diego Seoane <diegoseoanegonzalez8@gmail.com>
2021-03-25 17:06:10 +0100

    [Varios] Solucionado problema de transacciones en InscriptionService

--- backend/src/main/java/es/udc/paproject/backend/model/services/InscriptionServiceImpl.java
+++ backend/src/main/java/es/udc/paproject/backend/model/services/InscriptionServiceImpl.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
 
 @Service
+@Transactional
 public class InscriptionServiceImpl  implements InscriptionService{
 
     @Autowired
