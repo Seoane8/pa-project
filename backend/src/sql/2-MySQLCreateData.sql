2f6d8b43836212ad3e4128ebad4ed2c0652e4fed
Diego Seoane <diegoseoanegonzalez8@gmail.com>
2021-05-23 00:46:27 +0200

    [VARIOS] Actualizado script de creación de datos de la BD

--- backend/src/sql/2-MySQLCreateData.sql
+++ backend/src/sql/2-MySQLCreateData.sql
@@ -10,13 +10,13 @@ INSERT INTO SportTestType (name) VALUES ('Running');
 INSERT INTO SportTestType (name) VALUES ('Ciclismo');
 
 INSERT INTO SportTest (name, description, date, price, maxParticipants, numParticipants, sportTestTypeId, location, provinceId, rating, numRatings, version)
-    VALUES ('Prueba 1', 'Desc', '2020-07-01 10:00', 9.95, 500, 93, 1, 'A Coruña', 1, 0, 0, 0);
+    VALUES ('Prueba 1', 'Carrera popular por el centro histórico. 9Km', '2020-07-01 10:00', 9.95, 500, 93, 1, 'Santiago', 1, 0, 0, 0);
 INSERT INTO SportTest (name, description, date, price, maxParticipants, numParticipants, sportTestTypeId, location, provinceId, rating, numRatings, version)
-    VALUES ('Prueba 2', 'Desc', '2022-01-01 10:00', 9.95, 100, 0, 2, 'A Coruña', 1, 0, 0, 0);
+    VALUES ('Prueba 2', 'XXII Marcha ciclista Ferrolterra. 12Km', '2022-01-01 10:00', 9.95, 100, 0, 2, 'Ferrol', 1, 0, 0, 0);
 INSERT INTO SportTest (name, description, date, price, maxParticipants, numParticipants, sportTestTypeId, location, provinceId, rating, numRatings, version)
-    VALUES ('Prueba 3', 'Desc', '2022-02-01 10:00', 9.95, 300, 0, 1, 'Pontevedra', 2, 0, 0, 0);
+    VALUES ('Prueba 3', 'Carrera por la Ruta de los Cinco Miradores. 10Km', '2022-02-01 10:00', 9.95, 300, 0, 1, 'Marín', 2, 0, 0, 0);
 INSERT INTO SportTest (name, description, date, price, maxParticipants, numParticipants, sportTestTypeId, location, provinceId, rating, numRatings, version)
-    VALUES ('Prueba 4', 'Desc', '2022-03-01 10:00', 9.95, 2, 0, 2, 'Pontevedra', 2, 0, 0, 0);
+    VALUES ('Prueba 4', 'Marcha solidaria por el paseo marítimo. 8Km', '2022-03-01 10:00', 9.95, 2, 0, 2, 'Vigo', 2, 0, 0, 0);
 
 INSERT INTO User (userName, password, firstName, lastName, email, role)
     VALUES ('competitor1', '$2a$10$uicNGMO41KcyofyMPaCCwuQ89o1WM/wn5f9TGgCuFjZrfDLnECPyi', 'Juan', 'González', 'juan_gonzalez@mail.com', 0);
