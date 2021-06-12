88a2613487250626fe9025b5a325253e6065e735
Diego Seoane <diegoseoanegonzalez8@gmail.com>
2021-06-12 19:53:22 +0200

    [TT-INS-PRU] Añadida comprobación de numero de tarjeta vacía en prueba del formulario de inscripción

--- frontend/src/modules/inscription/components/InscriptionForm.test.js
+++ frontend/src/modules/inscription/components/InscriptionForm.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createStore } from 'redux'
 import { createMemoryHistory } from 'history'
-import { fireEvent, render } from '@testing-library/react'
+import { fireEvent, prettyDOM, render } from '@testing-library/react'
 import { Provider } from 'react-redux'
 import { IntlProvider } from 'react-intl'
 import { Router } from 'react-router-dom'
@@ -9,6 +9,7 @@ import { Router } from 'react-router-dom'
 import InscriptionForm from './InscriptionForm'
 import messages_en from '../../../i18n/messages/messages_en'
 import * as actions from '../actions'
+import userEvent from '@testing-library/user-event'
 
 const  generateRandomCardNumber = (len) => 
   Math.random().toString(10).substring(2,len+3)
@@ -51,7 +52,7 @@ describe('Rendering InscriptionForm component', () => {
     expect(cardNumberInput.value).toEqual(cardNumber)
   })
 
-  describe('Trying to inscribe for the sport test', () => {
+  describe('Trying to inscribe in the sport test', () => {
     let cardNumberInput
     let inscribeButton
 
@@ -60,72 +61,63 @@ describe('Rendering InscriptionForm component', () => {
       inscribeButton = component.queryAllByText(messages_en['project.global.buttons.inscribe'])[1]
     })
 
+    afterEach(() => actions.inscribe.mockRestore())
 
-    /*describe('by inserting wrong entries', () => {
-      test('when not insert card number, show validation error', () => {
-        fireEvent.click(inscribeButton)
-  
-        const validationError = component.getByText(messages_en['project.global.validator.cardNum'])
-        expect(validationError).toBeVisible()
-      })
-  
-      test('when insert short card number, show validation error', () => {
-        const cardNumber = generateRandomCardNumber(13)
-        
-        fireEvent.change(cardNumberInput, {target: {value: cardNumber}})
-        fireEvent.click(inscribeButton)
-  
-        const validationError = component.getByText(messages_en['project.global.validator.cardNum'])
-        expect(validationError).toBeVisible()
-      })
-    })*/
 
-    describe('by inserting correct entries', () => {
-      afterEach(() => actions.inscribe.mockRestore())
 
-      test('dispatch action correctly', () => {
-        const cardNumber = generateRandomCardNumber(16)
-        const inscribeSpy = jest.spyOn(actions, 'inscribe').mockImplementation(
-          (_sportTestId, _cardNumber, _onSuccess, _onErrors) => null
-        )
+    test('when not insert card number, not dispatch action', () => {
+      const inscribeSpy = jest.spyOn(actions, 'inscribe').mockImplementation(
+        (_sportTestId, _cardNumber, _onSuccess, _onErrors) => null
+      )
 
-        fireEvent.change(cardNumberInput, {target: {value: cardNumber}})
-        fireEvent.click(inscribeButton)
+      fireEvent.click(inscribeButton)
 
-        expect(inscribeSpy.mock.calls[0][0]).toEqual(sportTestId)
-        expect(inscribeSpy.mock.calls[0][1]).toEqual(cardNumber)
-      })
-
-      test('when the request is successful, change the path', () => {
-        const cardNumber = generateRandomCardNumber(16)
-        jest.spyOn(actions, 'inscribe').mockImplementation(
-          (_sportTestId, _cardNumber, onSuccess, _onErrors) => onSuccess()
-        )
-
-        fireEvent.change(cardNumberInput, {target: {value: cardNumber}})
-        fireEvent.click(inscribeButton)
-
-        expect(history).toHaveLength(2)
-        expect(history.location.pathname).toEqual('/inscription/inscribe-completed')
-      })
-
-      test('when the request fails, show backend errors', () => {
-        const cardNumber = generateRandomCardNumber(16)
-        const backendError = 'Some error has ocurred'
-        jest.spyOn(actions, 'inscribe').mockImplementation(
-          (_sportTestId, _cardNumber, _onSuccess, onErrors) => 
-            onErrors({globalError: backendError})
-        )
-
-        fireEvent.change(cardNumberInput, {target: {value: cardNumber}})
-        fireEvent.click(inscribeButton)
-        
-        const error = component.getByRole('alert')
-        expect(error).toHaveTextContent(backendError)
-        expect(history).toHaveLength(1)
-        expect(history.location.pathname).toEqual('/')
-      })
+      expect(inscribeSpy).not.toHaveBeenCalled()
     })
+
+    test('when insert card nuumber, dispatch action correctly', () => {
+      const cardNumber = generateRandomCardNumber(16)
+      const inscribeSpy = jest.spyOn(actions, 'inscribe').mockImplementation(
+        (_sportTestId, _cardNumber, _onSuccess, _onErrors) => null
+      )
+
+      fireEvent.change(cardNumberInput, {target: {value: cardNumber}})
+      fireEvent.click(inscribeButton)
+
+      expect(inscribeSpy.mock.calls[0][0]).toEqual(sportTestId)
+      expect(inscribeSpy.mock.calls[0][1]).toEqual(cardNumber)
+    })
+
+    test('when the request is successful, change the path', () => {
+      const cardNumber = generateRandomCardNumber(16)
+      jest.spyOn(actions, 'inscribe').mockImplementation(
+        (_sportTestId, _cardNumber, onSuccess, _onErrors) => onSuccess()
+      )
+
+      fireEvent.change(cardNumberInput, {target: {value: cardNumber}})
+      fireEvent.click(inscribeButton)
+
+      expect(history).toHaveLength(2)
+      expect(history.location.pathname).toEqual('/inscription/inscribe-completed')
+    })
+
+    test('when the request fails, show backend errors', () => {
+      const cardNumber = generateRandomCardNumber(16)
+      const backendError = 'Some error has ocurred'
+      jest.spyOn(actions, 'inscribe').mockImplementation(
+        (_sportTestId, _cardNumber, _onSuccess, onErrors) => 
+          onErrors({globalError: backendError})
+      )
+
+      fireEvent.change(cardNumberInput, {target: {value: cardNumber}})
+      fireEvent.click(inscribeButton)
+      
+      const error = component.getByRole('alert')
+      expect(error).toHaveTextContent(backendError)
+      expect(history).toHaveLength(1)
+      expect(history.location.pathname).toEqual('/')
+    })
+
   })
 
 })
